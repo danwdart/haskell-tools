235f04eee478159f625fc1ca993a53a439e69d09
Dan Dart <git@dandart.co.uk>
2022-10-16 16:19:25 +0100

    newer build tools as much as possible

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
 {
   nixpkgs ? import <nixpkgs> {},
+  haskellUpdates ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {},
   compiler ? "ghc942"
 }:
 let
@@ -12,20 +13,62 @@ let
 in
 compiler: rec {
   ghc = haskellPackages.${compiler};
+  toolsPerGHC = with ghc; {
+    ghc902 = {
+      ghcid = ghcid;
+      ghcide = ghcide;
+      haskell-language-server = haskell-language-server;
+      stylish-haskell = stylish-haskell;
+    };
+    ghc924 = {
+      ghcid = ghcid;
+      ghcide = ghcide;
+      haskell-language-server = haskell-language-server;
+      stylish-haskell = stylish-haskell;
+    };
+    ghc942 = {
+      ghcid = (ghc.override {
+        overrides = self: super: rec {
+          hspec-contrib = self.callCabal2nixWithOptions "hspec-contrib" (builtins.fetchGit {
+            url = "https://github.com/hspec/hspec.git";
+            rev = "734fd87231626bee978d34700505a11c53fff65c";
+          }) "--subpath hspec-contrib" {};
+        };
+      }).ghcid;
+      ghcide = (ghc.override {
+        overrides = self: super: rec {
+          ghc-check = lib.dontHaddock super.ghc-check;
+        };
+      }).ghcide;
+      # haskell-language-server = null;
+      haskell-language-server = haskellUpdates.haskell.packages.ghc942.haskell-language-server;
+      stylish-haskell = ghc924.stylish-haskell; # lib.enableCabalFlag ghc.stylish-haskell "ghc-lib";
+      #stylish-haskell = (ghc.override {
+      #  overrides = self: super: rec {
+      #    ormolu = lib.doJailbreak super.ormolu;
+      #    ghc-lib-parser = lib.doJailbreak (self.callHackage "ghc-lib-parser" "9.4.2.20220822" {});
+      #    hls-hlint-plugin = lib.doJailbreak super.hls-hlint-plugin;
+      #    fourmolu = lib.doJailbreak super.fourmolu;
+      #  };
+      # }).stylish-haskell;
+    };
+  };
   availableBuildTools = with ghc; {
     apply-refact                = ghc902.apply-refact;
     cabal-install               = cabal-install;
     doctest                     = ghc924.doctest;
-    # https://github.com/hspec/hspec/issues/747
-    ghcid                       = ghc924.ghcid;
-    ghcide                      = ghc902.ghcide;
+    # 0.5.1.1 not yet released
+    ghcid                       = toolsPerGHC.${compiler}.ghcid;
+    # Must be compiled using same version of ghc
+    ghcide                      = toolsPerGHC.${compiler}.ghcide;
     # Could not find module ‘GHCi.GhcApiCompat’
     ghci-dap                    = ghc924.ghci-dap;
     haskell-dap                 = haskell-dap;
     # https://github.com/hspec/hspec/issues/747
     haskell-debug-adapter       = ghc924.haskell-debug-adapter;
     # ghc-source-gen-0.4.3.0 broken
-    haskell-language-server     = ghc924.haskell-language-server;
+    # Must be compiled using same version of ghc
+    haskell-language-server     = toolsPerGHC.${compiler}.haskell-language-server; # 
     # text >=0.11 && <1.3
     hasktags                    = ghc924.hasktags;
     # ghc-lib-parser >=9.0 && <9.1, ghc-lib-parser-ex >=9.0.0.4 && <9.0.1
@@ -69,7 +112,7 @@ compiler: rec {
       rev = "70c14718486f399c11209580d4762b73499cd0e3";
     }) {});
     # base >=4.14 && <4.17, ghc-prim >0.2 && <0.9, time >=1.4 && <1.12
-    stylish-haskell             = ghc924.stylish-haskell;
+    stylish-haskell             = toolsPerGHC.${compiler}.stylish-haskell;
     weeder                      = ghc924.weeder;
   };
   defaultBuildTools = with availableBuildTools; [
