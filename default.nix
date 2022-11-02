ee1c7611e0480ca1d3f088f757934c94202e6913
Dan Dart <git@dandart.co.uk>
2022-11-02 13:28:13 +0000

    updates and fixes

--- default.nix
+++ default.nix
@@ -1,6 +1,5 @@
 {
   nixpkgs ? import <nixpkgs> {},
-  haskellUpdates ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/haskell-updates.tar.gz") {},
   compiler ? "ghc942"
 }:
 let
@@ -16,93 +15,71 @@ compiler: rec {
   toolsPerGHC = with ghc; {
     ghc902 = {
       ghcid = ghcid;
-      ghcide = ghcide;
-      haskell-language-server = haskell-language-server;
       stylish-haskell = stylish-haskell;
+      apply-refact = apply-refact;
+      haskell-debug-adapter = haskell-debug-adapter;
     };
     ghc924 = {
       ghcid = ghcid;
-      ghcide = ghcide;
-      haskell-language-server = haskell-language-server;
       stylish-haskell = stylish-haskell;
+      apply-refact = apply-refact;
+      haskell-debug-adapter = haskell-debug-adapter;
     };
     ghc942 = {
       ghcid = (ghc.override {
         overrides = self: super: rec {
-          hspec-contrib = self.callCabal2nixWithOptions "hspec-contrib" (builtins.fetchGit {
-            url = "https://github.com/hspec/hspec.git";
-            rev = "734fd87231626bee978d34700505a11c53fff65c";
-          }) "--subpath hspec-contrib" {};
+          # not in nix yet
+          hspec-contrib = self.callHackage "hspec-contrib" "0.5.1.1" {};
         };
       }).ghcid;
-      ghcide = (ghc.override {
+      # ghc-lib-parser: base >=4.14 && <4.17, ghc-prim >0.2 && <0.9, time >=1.4 && <1.12
+      stylish-haskell = ghc924.stylish-haskell;
+      # ghc-exactprint: base >=4.8 && <4.16, ghc >=7.10.2 && <9.2
+      apply-refact = ghc924.apply-refact;
+      haskell-debug-adapter = (ghc.override {
         overrides = self: super: rec {
-          ghc-check = lib.dontHaddock super.ghc-check;
+          # not in nix yet
+          hspec-contrib = self.callHackage "hspec-contrib" "0.5.1.1" {};
         };
-      }).ghcide;
-      haskell-language-server = (haskellUpdates.haskell.packages.ghc942.override {
-        overrides = self: super: rec {
-          # not yet released
-          streaming-commons = self.callCabal2nix "streaming-commons" (builtins.fetchGit {
-            url = "https://github.com/fpco/streaming-commons.git";
-            rev = "eb65c96c28e39a352023c24e2517880f0bf246c5";
-          }) {};
-        };
-      }).haskell-language-server;
-      stylish-haskell = ghc924.stylish-haskell; # lib.enableCabalFlag ghc.stylish-haskell "ghc-lib";
-      #stylish-haskell = (ghc.override {
-      #  overrides = self: super: rec {
-      #    ormolu = lib.doJailbreak super.ormolu;
-      #    ghc-lib-parser = lib.doJailbreak (self.callHackage "ghc-lib-parser" "9.4.2.20220822" {});
-      #    hls-hlint-plugin = lib.doJailbreak super.hls-hlint-plugin;
-      #    fourmolu = lib.doJailbreak super.fourmolu;
-      #  };
-      # }).stylish-haskell;
+      }).haskell-debug-adapter;
     };
   };
   availableBuildTools = with ghc; {
-    apply-refact                = ghc902.apply-refact;
-    cabal-install               = cabal-install;
-    doctest                     = ghc.callCabal2nix "doctest" (builtins.fetchGit {
-      url = "https://github.com/sol/doctest.git";
-      rev = "495a76478d63a31c61523b1a539f49340e6be122";
-    }) {};
-    # 0.5.1.1 not yet released
-    ghcid                       = toolsPerGHC.${compiler}.ghcid;
-    # Must be compiled using same version of ghc
-    ghcide                      = toolsPerGHC.${compiler}.ghcide;
-    # Could not find module ‘GHCi.GhcApiCompat’
-    ghci-dap                    = ghc924.ghci-dap;
+    # These must be compiled using same version of ghc
+    ghcide                      = ghcide;
+    haskell-language-server     = haskell-language-server;
+    # these don't matter quite so much
+    hoogle                      = hoogle;
+    implicit-hie                = implicit-hie;
+    ghci-dap                    = ghci-dap;
     haskell-dap                 = haskell-dap;
-    # https://github.com/hspec/hspec/issues/747
-    haskell-debug-adapter       = ghc924.haskell-debug-adapter;
-    haskell-docs-cli            = (ghc.override {
-      overrides = self: super: rec {
-        haskell-docs-cli = self.callCabal2nix "haskell-docs-cli" (builtins.fetchGit {
-          url = "https://github.com/lazamar/haskell-docs-cli.git";
-          rev = "6c40bd41f0f6be5f06afae2836c42710dc05cd87";
-        }) {};
-      };
-    }).haskell-docs-cli;
-    # ghc-source-gen-0.4.3.0 broken
-    # Must be compiled using same version of ghc
-    haskell-language-server     = toolsPerGHC.${compiler}.haskell-language-server; # 
+    selenium-server-standalone  = selenium-server-standalone;
+    cabal-install               = cabal-install;
+    doctest                     = doctest;
+    # stuff that has to be compiled differently depending on the ghc
+    apply-refact                = toolsPerGHC.${compiler}.apply-refact;
+    ghcid                       = toolsPerGHC.${compiler}.ghcid;
+    haskell-debug-adapter       = toolsPerGHC.${compiler}.haskell-debug-adapter;
+    stylish-haskell             = toolsPerGHC.${compiler}.stylish-haskell;
     # text >=0.11 && <1.3
     hasktags                    = ghc924.hasktags;
     # ghc-lib-parser >=9.0 && <9.1, ghc-lib-parser-ex >=9.0.0.4 && <9.0.1
     hlint                       = ghc924.hlint;
-    hoogle                      = hoogle;
-    implicit-hie                = implicit-hie;
-    # https://github.com/hspec/hspec/issues/747
-    krank                       = (ghc924.override {
+    # dhall 1.40.2: aeson >=1.0.0.0 && <2.1, template-haskell >=2.13.0.0 && <2.19
+    weeder                      = ghc924.weeder;
+    krank                       = (ghc.override {
       overrides = self: super: rec {
+        # not released yet
         PyF = self.callHackage "PyF" "0.11.1.0" {};
+        # not released yet
+        req = self.callHackage "req" "3.13.0" {};
       };
+    # updates to fix made since 0.2.3: https://github.com/guibou/krank/issues/94
     }).callCabal2nix "krank" (builtins.fetchGit {
       url = "https://github.com/guibou/krank.git";
       rev = "dd799efa1f2d1fac4ce0f80c4f47731b32e6fcaf";
     }) {};
-    selenium-server-standalone  = selenium-server-standalone;
+    # also: https://github.com/kowainik/slist/issues/55
     stan                        = lib.dontCheck ((ghc924.override {
       overrides = self: super: rec {
         # https://github.com/kowainik/extensions/issues/74
@@ -112,20 +89,11 @@ compiler: rec {
           rev = "6748ccbcea0d06488b6e288e9b68233fe4d73eb7";
         }) {});
         # https://github.com/kowainik/trial/issues/67
-        trial-tomland = lib.doJailbreak (self.callCabal2nixWithOptions "trial-tomland" (builtins.fetchGit {
-          url = "https://github.com/tomjaguarpaw/trial.git";
-          ref = "9.4";
-        }) "--subpath trial-tomland" {});
+        trial-tomland = lib.doJailbreak (lib.markUnbroken super.trial-tomland);
         clay = lib.doJailbreak super.clay;
         slist = lib.doJailbreak super.slist;
-        relude = (ghc924.override {
-          overrides = self: super: rec {
-            doctest = self.callCabal2nix "doctest" (builtins.fetchGit {
-              url = "https://github.com/sol/doctest.git";
-              rev = "495a76478d63a31c61523b1a539f49340e6be122";
-            }) {};
-          };
-        }).callHackage "relude" "1.1.0.0" {};
+        # relude 1.0.0.1: Module ‘Data.Semigroup’ does not export ‘Option(..)’ if using ghc942
+        # relude = lib.doJailbreak (self.callHackage "relude" "1.1.0.0" {});
       };
     # https://github.com/kowainik/stan/issues/423
     }).callCabal2nix "stan" (builtins.fetchGit {
@@ -133,9 +101,15 @@ compiler: rec {
       ref = "9.4-compat";
       rev = "70c14718486f399c11209580d4762b73499cd0e3";
     }) {});
-    # base >=4.14 && <4.17, ghc-prim >0.2 && <0.9, time >=1.4 && <1.12
-    stylish-haskell             = toolsPerGHC.${compiler}.stylish-haskell;
-    weeder                      = ghc924.weeder;
+    # not in hackage at all
+    haskell-docs-cli            = (ghc.override {
+      overrides = self: super: rec {
+        haskell-docs-cli = self.callCabal2nix "haskell-docs-cli" (builtins.fetchGit {
+          url = "https://github.com/lazamar/haskell-docs-cli.git";
+          rev = "6c40bd41f0f6be5f06afae2836c42710dc05cd87";
+        }) {};
+      };
+    }).haskell-docs-cli;
   };
   defaultBuildTools = with availableBuildTools; [
     apply-refact
