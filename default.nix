40f8c9d6dd9368d95e91c69324fb18297329eb36
Dan Dart <git@dandart.co.uk>
2023-02-05 12:22:38 +0000

    Simplify even more

--- default.nix
+++ default.nix
@@ -4,35 +4,30 @@
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-  haskell = nixpkgs.pkgs.haskell;
-  haskellPackages = haskell.packages;
-  lib = haskell.lib;
-  ghc94 = haskellPackages.ghc944;
-  ghc92 = haskellPackages.ghc92;
-  ghc90 = haskellPackages.ghc90;
+  haskellPackages = nixpkgs.haskellPackages;
 in
 compiler: rec {
-  ghc = haskellPackages.${compiler};
-  defaultBuildTools = with ghc; [
-    ghc92.apply-refact
-    ghc92.cabal-install
-    ghc92.doctest
-    ghc92.ghci-dap
-    ghc92.ghcid
-    ghc92.ghcide
-    ghc92.haskell-dap
-    ghc92.haskell-debug-adapter
-    ghc92.haskell-language-server
-    ghc92.hasktags
-    ghc92.hlint
-    ghc92.hoogle
-    ghc92.implicit-hie
-    ghc92.krank
+  ghc = nixpkgs.haskell.packages.${compiler};
+  defaultBuildTools = with haskellPackages; [
+    apply-refact
+    cabal-install
+    doctest
+    ghci-dap
+    ghcid
+    ghcide
+    haskell-dap
+    haskell-debug-adapter
+    haskell-language-server
+    hasktags
+    hlint
+    hoogle
+    implicit-hie
+    krank
     #stan
-    ghc92.stylish-haskell
-    ghc92.weeder
+    stylish-haskell
+    weeder
   ];
-  optionalBuildTools = with ghc; [
-    ghc92.selenium-server-standalone
+  optionalBuildTools = with haskellPackages; [
+    selenium-server-standalone
   ];
 }
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -euo pipefail
 
-nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc94 demo.nix --add-root result/nixpkgs/ghc94 --indirect) | cachix push haskell-tools
-nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc92 demo.nix --add-root result/nixpkgs/ghc92 --indirect) | cachix push haskell-tools
-nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc90 demo.nix --add-root result/nixpkgs/ghc90 --indirect) | cachix push haskell-tools
+nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc92 demo.nix --add-root result/nixpkgs/ghc92 --indirect) | cachix push dandart
+nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc92 demo.nix --add-root result/nixpkgs/ghc92 --indirect) | cachix push dandart
+nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc90 demo.nix --add-root result/nixpkgs/ghc90 --indirect) | cachix push dandart
