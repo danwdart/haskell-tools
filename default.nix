0f38fc6e601ec85b954f7e3e96407bb6780ce60b
Dan Dart <git@dandart.co.uk>
2024-10-11 20:02:51 +0100

    add back stan and add back a buildable version of krank

--- default.nix
+++ default.nix
@@ -4,7 +4,33 @@
 }:
 let
   gitignore = nixpkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  lib = nixpkgs.haskell.lib;
   haskellPackages = nixpkgs.haskellPackages;
+  perCompiler = {
+    ghc94 = {
+      krank = nixpkgs.haskell.packages.ghc94.callCabal2nix "krank" (builtins.fetchGit {
+        url = "https://github.com/guibou/krank.git";
+        ref = "main";
+      }) {};
+    };
+    ghc98 = {
+      krank = nixpkgs.haskell.packages.ghc98.callCabal2nix "krank" (builtins.fetchGit {
+        url = "https://github.com/guibou/krank.git";
+        ref = "main";
+      }) {};
+    };
+    ghc910 = {
+      krank = (nixpkgs.haskell.packages.ghc910.override {
+        overrides = self: super: rec {
+          pcre-heavy = lib.dontCheck super.pcre-heavy;
+          PyF = lib.dontCheck super.PyF;
+        };
+      }).callCabal2nix "krank" (builtins.fetchGit {
+        url = "https://github.com/guibou/krank.git";
+        ref = "main";
+      }) {};
+    };
+  };
 in
 compiler: rec {
   ghc = nixpkgs.haskell.packages.${compiler};
@@ -23,11 +49,14 @@ compiler: rec {
     hlint
     hoogle
     hpack
-    # hpack-convert # broken in all versions
+    # (ghc.callCabal2nix "krank" (builtins.fetchGit {
+    #   url = "https://github.com/yamadapc/hpack-convert.git";
+    #   ref = "hpack-convert";
+    # }) {}) # broken in all versions
     implicit-hie
-    # krank # broken
+    perCompiler.${compiler}.krank # broken
     stack
-    # stan # broken in all versions
+    stan # broken in all versions
     stylish-haskell
     weeder
   ];
--- push.sh
+++ push.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc94 demo.nix --add-root result/nixpkgs/ghc94 --indirect) | cachix push dandart
 # nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc94 demo.nix --add-root result/nixpkgs/ghc94 --indirect) | xargs nix-copy-closure --gzip -s --include-outputs dwd@cache.jolharg.com
-nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc96 demo.nix --add-root result/nixpkgs/ghc96 --indirect) | cachix push dandart
-# nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc96 demo.nix --add-root result/nixpkgs/ghc96 --indirect) | xargs nix-copy-closure --gzip -s --include-outputs dwd@cache.jolharg.com
 nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc98 demo.nix --add-root result/nixpkgs/ghc98 --indirect) | cachix push dandart
 # nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc98 demo.nix --add-root result/nixpkgs/ghc98 --indirect) | xargs nix-copy-closure --gzip -s --include-outputs dwd@cache.jolharg.com
 nix-store -qR --include-outputs $(nix-instantiate --argstr compiler ghc910 demo.nix --add-root result/nixpkgs/ghc910 --indirect) | cachix push dandart
